d5ac9fd4892041dfe1c424de8dc46a3807d49047
devraj thakur <somd1051@gmail.com>
2025-03-23 01:29:04 +0530

    new

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -21,76 +21,8 @@ router.post('/create', authGuard ,async (req, res) => {
 });
 
 
-router.post('/list', authGuard,async (req, res) => {
-  const page = req.body.pagenation || 1;
-  const limit = 8;
-  const skip = (page - 1) * limit;
-
-  try {
-  
-    const tasks = await Task.find({assignedTo: req.user} ).skip(skip).limit(8).populate('assignedTo');
-    
-    res.status(200).json({tasks});
-   
-
-  } catch (err) {
-    res.status(500).send('Error fetching tasks');
-  }
-});
-router.post('/list/:priority', authGuard,async (req, res) => {
-  const page = req.body.pagenation || 1;
-  const limit = 8;
-  const skip = (page - 1) * limit;
-
-  try {
-    
-    const tasks = await Task.find({ assignedTo: req.user }).where("priority",req.params.priority).populate('assignedTo').skip(skip).limit(8);
-    
-    
-    res.status(200).json({tasks});
-   
-
-  } catch (err) {
-    res.status(500).send('Error fetching tasks');
-  }
-});
 
 
-router.get('/taskdata/:id', async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id).populate('assignedTo');
- 
 
-    res.status(201).json({ task });
-  } catch (err) {
-    res.status(500).send('Error fetching task details');
-  }
-});
-
-
-router.post('/update/:id', async (req, res) => {
-  try {
-    const { title, description, dueDate, priority, status } = req.body;
-   const task= await Task.findByIdAndUpdate(req.params.id, { title, description, dueDate, priority, status });
-   await task.save();
-    res.status(200).json(task);
-  } catch (err) {
-    res.status(500).send('Error updating task');
-  }
-});
-
-
-router.get('/delete/:id', async (req, res) => {
-  try {
-   const task= await Task.findByIdAndDelete(req.params.id);
-     await   User.updateOne(
-          { _id: task.assignedTo }, 
-          { $pull: { tasks: req.params.id } } 
-        )
-    res.status(200);
-  } catch (err) {
-    res.status(500).send('Error deleting task');
-  }
-});
 
 export default router;
